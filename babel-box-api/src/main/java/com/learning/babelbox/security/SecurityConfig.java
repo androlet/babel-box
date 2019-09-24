51729f76729932c39a43d4358651f99ed696601e
Thomas <thomas.lecomte25@gmail.com>
2019-09-24 23:40:23 +0200

    Configure CSRF security

--- babel-box-api/src/main/java/com/learning/babelbox/security/SecurityConfig.java
+++ babel-box-api/src/main/java/com/learning/babelbox/security/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+                .csrf().ignoringAntMatchers(LOGIN_URL).disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .cors().disable()
