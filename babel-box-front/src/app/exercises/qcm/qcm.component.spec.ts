9a66438330c3d6cac478f5fb2b4df30bbfbe8a0c
Thomas <thomas.lecomte25@gmail.com>
2019-11-05 00:15:49 +0100

    implement qcm

--- babel-box-front/src/app/exercises/qcm/qcm.component.spec.ts
+++ babel-box-front/src/app/exercises/qcm/qcm.component.spec.ts
@@ -2,6 +2,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ExercisesModuleTesting, QcmComponentTested } from '../exercises.module.testing';
 import { QcmComponent } from './qcm.component';
+import { QCM_TRANSLATIONS__ANSWER_EQ_2 } from '../exercise-translations/mock-exo';
+import { QcmOption } from '../domain/exercises';
 
 describe('QcmComponent', () => {
   let component: QcmComponent;
@@ -22,5 +24,21 @@ describe('QcmComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(component.selectedOption).toBe(undefined);
+    expect(component.isDescriptionDisplayed()).toBe(false);
+  });
+
+  it('should display option description for correction WHEN a qcm option has been selected', () => {
+    //given
+    const selectedOption = QCM_TRANSLATIONS__ANSWER_EQ_2.options[0];
+    component.questionExo = QCM_TRANSLATIONS__ANSWER_EQ_2;
+
+    //when
+    component.selectOption(selectedOption);
+
+    //then
+    expect(component.selectedOption).toBe(selectedOption);
+    expect(component.isDescriptionDisplayed()).toBe(true);
+    expect(component.getOptionTranslation()).toBe(selectedOption.translation);
   });
 });
--- babel-box-front/src/app/exercises/qcm/qcm.component.ts
+++ babel-box-front/src/app/exercises/qcm/qcm.component.ts
@@ -1,18 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { QCM_TRANSLATIONS__ANSWER_EQ_2 } from '../exercise-translations/mock-exo';
+import { Component, OnInit, Input } from '@angular/core';
+import { BaseComponent } from 'src/app/base.component';
+import { QcmOption, QcmExercise } from '../domain/exercises';
+import { TranslationResults } from 'src/app/search/domain/translation-results';
 
 @Component({
   selector: 'app-qcm',
   templateUrl: './qcm.component.html',
   styleUrls: ['./qcm.component.css']
 })
-export class QcmComponent implements OnInit {
+export class QcmComponent extends BaseComponent implements OnInit {
 
-  questionExo = QCM_TRANSLATIONS__ANSWER_EQ_2;
+  @Input() questionExo: QcmExercise;
+  selectedOption: QcmOption;
 
-  constructor() { }
+  constructor() {
+    super();
+  }
 
   ngOnInit() {
+  }
+
+  selectOption(qcmOption: QcmOption): void {
+    this.selectedOption = qcmOption;
+  }
+
+  isDescriptionDisplayed(): boolean {
+    return !!this.selectedOption;
+  }
+
+  getOptionTranslation(): TranslationResults {
+    return this.selectedOption.translation;
   }
 
 }
